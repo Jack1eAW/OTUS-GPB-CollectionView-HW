078d11642eaecb1e96140c306666003db5cb8b38
Jack1eAW <alcheb2014@gmail.com>
2022-10-29 08:44:39 +0300

    Initial Commit

---
+++ OTUS-GPB-CollectionView-HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OTUS-GPB-CollectionView-HW
+//
+//  Created by Aleksandr Chebotarev on 10/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
